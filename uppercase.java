3e1adc0277eb9cae998fa28df2e82bda890a5e31
MUNNAKUMAR727762 <98367773+MUNNAKUMAR727762@users.noreply.github.com>
2023-06-07 21:52:35 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,17 @@
+
+package star;
+
+import java.util.*;
+public class uppercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String st=str.toUpperCase();
+		System.out.println(st);
+		
+
+	}
+
+}
